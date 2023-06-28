ad5516faf9eb502799fd6ef4c299b2bea84d0087
ElectronicEXE <114730703+ElectronicEXE@users.noreply.github.com>
2023-06-28 14:14:41 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,13 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 import serial
-import threading
-import time
+from draw import *
 
 serial_port = 'COM21'
 baud_rate = 9600
+
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
+ready = False
 
 c1 = 100
 c2 = 100
@@ -18,46 +19,25 @@ c6 = 100
 c7 = 100
 c8 = 100
 
+c1l = 100
+c2l = 100
+c3l = 100
+c4l = 100
+c5l = 100
+c6l = 100
+c7l = 100
+c8l = 100
+
 pos = 0
 
-white = 255,255,255
-
 def main():
-    pygame.init()
-    
-    
-    DISPLAY=pygame.display.set_mode((570,150),0,32)
-
-    BLACK=(30,30,50)
-
-    DISPLAY.fill(BLACK)
-
+    pygame_init()
  
     while True:
-        font = pygame.font.SysFont("Arial", 30)
-        text = font.render("   position:  " + str(pos) + "    ", True, white,BLACK)
-        textRect = text.get_rect()
-        textRect.center = (285 , 130 )
-        serGet()
-        pygame.display.update()
-        c1rgb = c1,c1,c1
-        c2rgb = c2,c2,c2
-        c3rgb = c3,c3,c3
-        c4rgb = c4,c4,c4
-        c5rgb = c5,c5,c5
-        c6rgb = c6,c6,c6
-        c7rgb = c7,c7,c7
-        c8rgb = c8,c8,c8
 
-        pygame.draw.rect(DISPLAY,c1rgb,(10,10,60,80))
-        pygame.draw.rect(DISPLAY,c2rgb,(80,10,60,80))
-        pygame.draw.rect(DISPLAY,c3rgb,(150,10,60,80))
-        pygame.draw.rect(DISPLAY,c4rgb,(220,10,60,80))
-        pygame.draw.rect(DISPLAY,c5rgb,(290,10,60,80))
-        pygame.draw.rect(DISPLAY,c6rgb,(360,10,60,80))
-        pygame.draw.rect(DISPLAY,c7rgb,(430,10,60,80))
-        pygame.draw.rect(DISPLAY,c8rgb,(500,10,60,80))
-        DISPLAY.blit(text,textRect)
+        serGet(ser)
+        draw_text(DISPLAY, ready, c1l, c2l, c3l, c4l, c5l, c6l, c7l, c8l, pos)
+        draw_box(DISPLAY, c1, c2, c3, c4, c5, c6, c7, c8)
 
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -65,10 +45,10 @@ def main():
                 sys.exit()
         
 
-def serGet():
+def serGet(ser):
     data = ser.readline().decode('utf-8').strip()
     splitData = data.split(",")
-    global c1l,c2l,c3l,c4l,c5l,c6l,c7l,c8l,pos
+    global c1l,c2l,c3l,c4l,c5l,c6l,c7l,c8l,pos,ready
     try:
         c1l = splitData[0]
         c2l = splitData[1]
@@ -80,10 +60,11 @@ def serGet():
         c8l = splitData[7]
         pos = splitData[8]
         setcolor()            
+        ready = True
     except:
         print("not ready")
         print("calibratione is still in progress .....................")
-
+        ready = False
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     leftSpan = leftMax - leftMin
@@ -93,7 +74,6 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
 
 def setcolor():
     global c1,c2,c3,c4,c5,c6,c7,c8
-
     c1 = int(translate(int(c1l), 1000, 0, 0, 255))
     c2 = int(translate(int(c2l), 1000, 0, 0, 255))
     c3 = int(translate(int(c3l), 1000, 0, 0, 255))
@@ -102,6 +82,15 @@ def setcolor():
     c6 = int(translate(int(c6l), 1000, 0, 0, 255))
     c7 = int(translate(int(c7l), 1000, 0, 0, 255))
     c8 = int(translate(int(c8l), 1000, 0, 0, 255))
+    return c1,c2,c3,c4,c5,c6,c7,c8
+
+
+def pygame_init():
+    pygame.init()
+    global DISPLAY
+    DISPLAY=pygame.display.set_mode((570,150),0,32)
+    BLACK=(30,30,50)
+    DISPLAY.fill(BLACK)
 
 if __name__ == '__main__':
     main()
