8e40a612265f36d77432a4789336de3dc816d0bf
ElectronicEXE <114730703+ElectronicEXE@users.noreply.github.com>
2023-06-29 13:08:04 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,43 +1,41 @@
 import pygame, sys
 from pygame.locals import *
 import serial
-from draw import *
+
+# VERSION = 1.7
+
+#################################################
+Sensor_Count = 8
 
 serial_port = 'COM21'
-baud_rate = 9600
+
+baud_rate = 115200
+#################################################
+
 
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
 ready = False
 
-c1 = 100
-c2 = 100
-c3 = 100
-c4 = 100
-c5 = 100
-c6 = 100
-c7 = 100
-c8 = 100
-
-c1l = 100
-c2l = 100
-c3l = 100
-c4l = 100
-c5l = 100
-c6l = 100
-c7l = 100
-c8l = 100
+clist =[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100]
+cLlist =[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100]
 
 pos = 0
 
+
+x = Sensor_Count * 71
+if x < 300:
+    x = 500
+y = 150
+
 def main():
-    pygame_init()
+    pygame_init(x,y)
  
     while True:
 
         serGet(ser)
-        draw_text(DISPLAY, ready, c1l, c2l, c3l, c4l, c5l, c6l, c7l, c8l, pos)
-        draw_box(DISPLAY, c1, c2, c3, c4, c5, c6, c7, c8)
+        draw_text(DISPLAY, ready,cLlist, pos,x,Sensor_Count)
+        draw_box(DISPLAY,clist, Sensor_Count)
 
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -48,17 +46,15 @@ def main():
 def serGet(ser):
     data = ser.readline().decode('utf-8').strip()
     splitData = data.split(",")
-    global c1l,c2l,c3l,c4l,c5l,c6l,c7l,c8l,pos,ready
+    global cLlist,pos,ready,Sensor_Count
     try:
-        c1l = splitData[0]
-        c2l = splitData[1]
-        c3l = splitData[2]
-        c4l = splitData[3]
-        c5l = splitData[4]
-        c6l = splitData[5]
-        c7l = splitData[6]
-        c8l = splitData[7]
-        pos = splitData[8]
+        ii = 0
+        for i in range(Sensor_Count):
+            cLlist[ii] = splitData[ii]
+            ii = ii + 1
+        pos = splitData[Sensor_Count]
+        if len(splitData) != Sensor_Count+1:
+            print(f"WARNING: SENSOR COUNT MISSMATCHING. programmed sensor count: {Sensor_Count} received sensor count: {len(splitData)-1}")
         setcolor()            
         ready = True
     except:
@@ -73,24 +69,59 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     return rightMin + (valueScaled * rightSpan)
 
 def setcolor():
-    global c1,c2,c3,c4,c5,c6,c7,c8
-    c1 = int(translate(int(c1l), 1000, 0, 0, 255))
-    c2 = int(translate(int(c2l), 1000, 0, 0, 255))
-    c3 = int(translate(int(c3l), 1000, 0, 0, 255))
-    c4 = int(translate(int(c4l), 1000, 0, 0, 255))
-    c5 = int(translate(int(c5l), 1000, 0, 0, 255))
-    c6 = int(translate(int(c6l), 1000, 0, 0, 255))
-    c7 = int(translate(int(c7l), 1000, 0, 0, 255))
-    c8 = int(translate(int(c8l), 1000, 0, 0, 255))
-    return c1,c2,c3,c4,c5,c6,c7,c8
+    global clist,cLlist
+    iii = 0
+    for i in range(Sensor_Count):
+        clist[iii] = int(translate(int(cLlist[iii]), 1000, 0, 0, 255))
+        iii = iii +1
 
 
-def pygame_init():
+def draw_box(DISPLAY, clist, S_no):
+        i = 0
+        w = 10
+        for i in range(S_no):
+            rgb = clist[i], clist[i], clist[i]
+            pygame.draw.rect(DISPLAY,rgb,(w,10,60,80))
+            w = w+70
+
+        pygame.display.update()   
+
+
+def draw_text(DISPLAY, ready, cLlist, pos,x,S_no):
+        white = 255,255,255
+        BLACK=(30,30,50)
+        font = pygame.font.SysFont("Arial", 30)
+        font2 = pygame.font.SysFont("Arial", 15)
+        if ready ==True:
+            text = font.render("                   position:  " + str(pos) + "                    ", True, white,BLACK)
+        else:
+            text = font.render("calibration is still in progress ...", True, white,BLACK)
+
+        textRect = text.get_rect()
+        textRect.center = (x/2 , 130 )
+        i = 0
+        w = 40
+        for i in range (S_no):
+            
+            text1 = font2.render("  " + str(cLlist[i]) + "  ", True, white,BLACK)
+            textRect1 = text1.get_rect()
+            textRect1.center = (w ,100 )
+            DISPLAY.blit(text1,textRect1)
+            w = w + 70
+        
+
+       
+        DISPLAY.blit(text,textRect)
+
+
+
+def pygame_init(x,y):
     pygame.init()
     global DISPLAY
-    DISPLAY=pygame.display.set_mode((570,150),0,32)
+    DISPLAY=pygame.display.set_mode((x,y),0,32)
     BLACK=(30,30,50)
     DISPLAY.fill(BLACK)
+    pygame.display.set_caption('QTR-viewer')
 
 if __name__ == '__main__':
     main()
